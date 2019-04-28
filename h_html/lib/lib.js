81fd20dd9d121b95ba8d31e8c45e1ea0cbebd7ef
unknown <huangfeng191@163.com>
2019-04-28 14:52:32 +0800

    12

--- h_html/lib/lib.js
+++ h_html/lib/lib.js
@@ -1,7 +1,7 @@
 //jQuery扩展
 jQuery.extend({
     GetTemplate: function (fn) {
-        debugger
+        
         return fn.toString().replace(/^function\s*\(\s*\)\s*\{\s*\/\*/g, "").replace(/\*\/;?\s*\}$/g, "");
     },
     ImplantTemplate: function (Id, Temp) {
