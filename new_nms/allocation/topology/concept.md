614ab752f026be049effa546baebb448cafa7792
jinjianwu <huangfeng191@163.com>
2019-06-06 08:46:48 +0800

    $message

---
+++ new_nms/allocation/topology/concept.md
@@ -0,0 +1,10 @@
+<!-- pathType  2： physical  line   1: logic line  -->
+
+1:  zNe   aNe saved base on device id ;
+
+/nms/spring/viewData/deletePathByObject
+
+
+添加设备:
+
+"addType": "2",   // 逻辑拓扑  "1" 物理拓扑
--- new_nms/allocation/topology/logic/dustbin.js
+++ new_nms/allocation/topology/logic/dustbin.js
@@ -1,12 +1,31 @@
  // /nms/spring/viewData/addPath
-{
-    "path": {
-        "aNe": "ccf837a3-f852-4c70-a1da-c4835a423d56",
-        "zNe": "db0ec7d6-69d0-44a3-9a09-746709b63287",
-        "pathName": "白广路GNSS97_PRC,北京白广路_PRC",
-        "pathType": "1",
-        "remark": ""
-    },
-    "pathLinkList": [],
-    "token": "C59D106E679DB12F561E943549257781"
-}
+ addPath = {
+     "path": {
+         "aNe": "ccf837a3-f852-4c70-a1da-c4835a423d56",
+         "zNe": "db0ec7d6-69d0-44a3-9a09-746709b63287",
+         "pathName": "白广路GNSS97_PRC,北京白广路_PRC",
+         "pathType": "1",
+         "remark": ""
+     },
+     "pathLinkList": [],
+     "token": "C59D106E679DB12F561E943549257781"
+ }
+
+
+ addLogicDevice
+     = {
+         "areaId": "117c4c9f-4e09-462b-9c6e-fbb5d8be5506",
+         "name": "白广路GNSS97_PRC",
+         "neType": "PRC",
+         "deviceType": "GNSS97",
+         "ip": "192.11.1.1",
+         "vendor": "dt",
+         "owner": "",
+         "description": "",
+         "aliasName": "",
+         "phyX": "157px",  // 设备变更的时候 会影响
+         "phyY": "3px",    // 设备变更的时候 会影响 
+         "addType": "2",   // 逻辑拓扑
+         "token": "A5FC8DBCF0FD6423E266D57D53A84CBD",
+         "port": 0
+     }
