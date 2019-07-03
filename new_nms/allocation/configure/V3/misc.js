410c553e60cf07c2df4cc3fb270c81bad55b23bb
jinjianwu <huangfeng191@163.com>
2019-07-03 11:13:33 +0800

    1

--- new_nms/allocation/configure/V3/misc.js
+++ new_nms/allocation/configure/V3/misc.js
@@ -1,9 +1,12 @@
 LCIM :line clock  input measure 
 
 
-input 
+input  //fold mode
 CHAN-MEAS
 CHANMEAS : Y 测试 N 不进行测试
+
+输入信道只能时 1 跟2 槽位
+1- 10 可做测试信道
 
 CSCLK1～ CSCLK4
 CLCIM1-1~CLCIM1-8
@@ -27,42 +30,47 @@ LOS恢复时间  SET-LOS-INP LOStime // 1～100
 
 
 
-1、系统相关功能：
-        工作模式：SET-PRMTR-SYS, (TYPE=WORKMODE)
-                      (查询 RTRV-PRMTR-SYS)
-        多数表决控制：SET-PRMTR-SYS, (TYPE=VOTECTRL)
-                     （查询 RTRV-PRMTR-SYS）
-      
-        LCIM板卡类型：SET-PRMTR-LCIM，（PRMTRTYPE=TYPE）
-                      (查询RTRV-STATE-LCIM， cmdtype=TYPE)
-	 TSOU输出备份方式：SET-PRMTR-TSOU (PRMTRTYPE=TYPE)
-                       (查询 RTRV-STATE-TSOU)
-         板卡运行状态：RTRV-COND-EQPT
+1、系统相关功能 
+
+工作模式 SET-PRMTR-SYS,    //VALUE MAN|INT1  (TYPE=WORKMODE) (查询 RTRV-PRMTR-SYS)
+多数表决控制 SET-PRMTR-SYS, //VALUE Y|N   (TYPE=VOTECTRL) （查询 RTRV-PRMTR-SYS）
+LCIM板卡类型 SET-PRMTR-LCIM,（PRMTRTYPE=TYPE） (查询RTRV-STATE-LCIM， cmdtype=TYPE) // <aid>：取值为BLCIM1～BLCIMA。 ?
+TSOU输出备份方式 SET-PRMTR-TSOU (PRMTRTYPE=TYPE) (查询 RTRV-STATE-TSOU) //<aid>：取值为BTSOU11～BTSOU4A。
+
+// 板卡运行状态 RTRV-COND-EQPT
 
 
 
 
-2、钟卡相关功能：
-               时钟卡：SRCU1/SOCU1/SRCU2/SOCU2
-         锁时状态延时：SET-PRMTR-SCLK, (PRMTRTYPE=LOCKDELAY)
-           源切换延时：
-         锁相状态延时：
-           卫星卡类型：SET-PRMTR-SCLK, (PRMTRTYPE=SCARDTYPE)
-       卫星卡工作模式：SET-PRMTR-SCLK, (PRMTRTYPE=WM)
-         可视卫星门限：SET-PRMTR-SCLK, (PRMTRTYPE=SATNUMTH)
-             仰角门限：SET-PRMTR-SCLK, (PRMTRTYPE=SATELETH)
-                 位置：SET-PRMTR-SCLK, (PRMTRTYPE=SCARDPOS)
+
+
+// 在独立模式下 可以设置 group 
+2、钟卡相关功能 
+时钟卡 SRCU1/SOCU1/SRCU2/SOCU2
+锁时状态延时 SET-PRMTR-SCLK, (PRMTRTYPE=LOCKDELAY)  //<VALUE>值域为3～86400，表示设置时钟板锁时状态的延迟时间。
+源切换延时                                        //<VALUE2>值域为10～86400，表示源切换的延迟时间。
+锁相状态延时                                        //<VALUE3>值域为90～86400，表示设置时钟板锁相状态的延迟时间。
+卫星卡类型 SET-PRMTR-SCLK, (PRMTRTYPE=SCARDTYPE)  //<VALUE>取值为GLOGPS|GPS| BEIDOU|NONE，表示配置卫星卡为：双星卡|GPS卡| 北斗卡|无卫星卡。
+卫星卡工作模式 SET-PRMTR-SCLK, (PRMTRTYPE=WM) //<VALUE>：取值为DUAL|GPS|GLO|BD，表示配置双星卡的工作模式为混合|| GPS|GLONASS工作模式| 北斗卡工作模式。
+                                             //– <VALUE2>：取值为GPS|GLO|BD，表示配置双星卡的时间基准为GPS| GLONASS| BEIDOU。
+可视卫星门限 SET-PRMTR-SCLK, (PRMTRTYPE=SATNUMTH) //取值为SATNUMTH表示配置卫星卡的可视卫星门限。 <VALUE>值域为0～8
+仰角门限 SET-PRMTR-SCLK, (PRMTRTYPE=SATELETH) //取值为SATELETH表示配置卫星卡仰角门限。 <VALUE>值域为0～10
+位置 SET-PRMTR-SCLK, (PRMTRTYPE=SCARDPOS) // <PRMTRTYPE>：取值为SCARDPOS表示卫星卡的位置信息
+                                          //– <VALUE>： <VALUE>表示设置卫星卡的纬度信息。
+                                          //– <VALUE2>： <VALUE2>表示设置卫星卡的经度信息。
+                                          //– <VALUE3>：表示设置卫星卡的高度信息。
 
 
 
-4、测试相关功能：
-             信号类型：SET-CHANPM-LCIM, CMDTYPE=CHANTYPE
-                      (查询 RTRV-PRMTR-LCIM)
-             测试使能：SET-CHANPM-LCIM, CMDTYPE=CHANMEAS
-         通道测试模板：SET-PMTH-INP, montype=WG/WT/FT
+4、测试相关功能  //<aid>：取值为CLCIM1-1～CLCIMA-8   
+信号类型 SET-CHANPM-LCIM, CMDTYPE=CHANTYPE  //<CMDTYPE> 为CHANTYPE。 <VALUE>：取值为10M|5M|1M|2048K|CC|E1|NONE，  // (查询 RTRV-PRMTR-LCIM)
+测试使能 SET-CHANPM-LCIM, CMDTYPE=CHANMEAS  // <CMDTYPE>为CHANMEAS。 <VALUE>取值为Y | N，代表进行测试和不进行测试。 
+通道测试模板 SET-PMTH-INP, montype=WG/WT/FT //<montype>：取值为WG|WE|WT|WJ|FT|G812。
 
-5、输出相关功能：
-          端口使能：SET-CHPRMTR-TSOU (CHPMTYPE=CHANBLOCK)
-                    (查询 RTRV-STATE-TSOU)
-          信号类型：SET-CHPRMTR-TSOU (CHPMTYPE=CHANTYPE
-                    (查询 RTRV-STATE-TSOU)
+
+
+
+5、输出相关功能 
+{/* <aid>：取值为GTSOU10-01～GTSOU45-20。 */}
+端口使能 SET-CHPRMTR-TSOU (CHPMTYPE=CHANBLOCK) //<CHPMTYPE>取值为CHANBLOCK，表示设置通道信号关闭打开状态。 <VALUE>取值为CLOSE|OPEN ，代表打开或关闭TSOU通道。  (查询 RTRV-STATE-TSOU)
+信号类型 SET-CHPRMTR-TSOU (CHPMTYPE=CHANTYPE // <CHPMTYPE>取值为CHANTYPE，表示设置通道信号类型。 <VALUE>取值为10M|5M|1M|2048K|1544K|E1|T1。  (查询 RTRV-STATE-TSOU)
