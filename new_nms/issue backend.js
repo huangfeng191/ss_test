aa36b9aef7bc7cadd00ef51eb85f899353b11885
jinjianwu <huangfeng191@163.com>
2019-07-27 16:09:25 +0800

    admin

--- new_nms/issue backend.js
+++ new_nms/issue backend.js
@@ -1,1 +1,12 @@
 // /nms/spring/systemManage/serverHardware   HTTP Status 500 – Internal Server Error
+
+
+
+// 177  sm2000 设备  192.168.1.33 实际在线,web显示离线 
+
+
+//用户管理：
+    /nms/spring/user/pages  用户查询  搜索条件  中文无法正常删选 ，
+    设备： 返回结果不对： 一个用户返回多条
+    登录状态 ： 未登录情况下，接口报错
+    区域 /nms/spring/user/getAreas 核实 获取用户区域列表接口是否准确
