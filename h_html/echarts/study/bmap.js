3619687d2fb6b2789e4fad3d5221e2fcfc0439fe
jinjianwu <huangfeng191@163.com>
2019-08-03 17:11:02 +0800

    admin

--- h_html/echarts/study/bmap.js
+++ h_html/echarts/study/bmap.js
@@ -1,4 +1,69 @@
 
+//可以选择的图表
+// 1 setRemark   :  "可选择分类": 属性
+
+var remarks={
+  "business":{
+    default:true
+  },
+  "sample":{
+    // default:true
+  },
+  "scatter":{
+    
+  }
+    
+  }
+
+
+
+
+
+  optionBusiness={
+    // 加载最简单的地图
+      // 加载 bmap 组件
+      bmap: {
+          // 百度地图中心经纬度
+          center: [120.13066322374, 30.240018034923],
+          // 百度地图缩放
+          zoom: 5, // 
+          // 是否开启拖拽缩放，可以只设置 'scale' 或者 'move'
+          roam: true,
+          // 百度地图的自定义样式，见 http://developer.baidu.com/map/jsdevelop-11.htm
+          mapStyle: mapStyle
+      },
+      series: [{
+          type: 'scatter',
+          // 使用百度地图坐标系
+          coordinateSystem: 'bmap',
+          // 数据格式跟在 geo 坐标系上一样，每一项都是 [经度，纬度，数值大小，其它维度...]
+          data: [ [120, 30, 1] ]
+      }]
+  }
+  optionSample={
+    // 加载最简单的地图
+      // 加载 bmap 组件
+      bmap: {
+          // 百度地图中心经纬度
+          center: [120.13066322374, 30.240018034923],
+          // 百度地图缩放
+          zoom: 5,
+          // 是否开启拖拽缩放，可以只设置 'scale' 或者 'move'
+          roam: true,
+          // 百度地图的自定义样式，见 http://developer.baidu.com/map/jsdevelop-11.htm
+          mapStyle: {}
+      },
+      series: [{
+          type: 'scatter',
+          // 使用百度地图坐标系
+          coordinateSystem: 'bmap',
+          // 数据格式跟在 geo 坐标系上一样，每一项都是 [经度，纬度，数值大小，其它维度...]
+          data: [ [120, 30, 1] ]
+      }]
+  }
+
+
+
 var data = [
   {name: '海门', value: 911},
   {name: '鄂尔多斯', value: 12},
@@ -37,7 +102,7 @@ var convertData = function (data) {
 
 
 
-optionSample = {
+optionScatter = {
   // backgroundColor: '#404a59',
   title: {
       text: '全国主要城市空气质量',
@@ -55,6 +120,7 @@ optionSample = {
       center: [104.114129, 37.550339],
       zoom: 5,
       roam: true,
+      mapStyle:{}
   },
   series : [
       {
@@ -89,41 +155,3 @@ optionSample = {
 
 
 
-
-//可以选择的图表
-// 1 setRemark   :  "可选择分类": 属性
-
-var remarks={
-"sample":{
-
-},
-"sample1":{
-  default:true
-}
-  
-}
-
-
-optionSample1= {
-  xAxis: {
-    type: 'category',
-    data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie']
-  },
-  yAxis: {},
-  series: [{
-      type: 'bar',
-      name: '2015',
-      data: [89.3, 92.1, 94.4, 85.4]
-    },
-    {
-      type: 'bar',
-      name: '2016',
-      data: [95.8, 89.4, 91.2, 76.9]
-    },
-    {
-      type: 'bar',
-      name: '2017',
-      data: [97.7, 83.1, 92.5, 78.1]
-    }
-  ]
-}
