b62c58c5a8685069ac9cb91ed5e4db1e9d7ff988
jinjianwu <huangfeng191@163.com>
2019-09-04 14:06:40 +0800

    12

--- a_test/t.js
+++ a_test/t.js
@@ -1,3 +1,17 @@
-position: absolute;
-right: 5px;
-top: 5px;
+a = {
+    "code": 100000,
+    "data": {
+        "deviceType": "\"TimeProvider\"",
+        "sysMode": "",
+        "gpsLocation": "AUTO,0-0-0.0N,0-0-0.0E,0.",
+        "frameInfoList": [],
+        "alarmNum": 0,
+        "systemTime": "",
+        "deviceName": "衡供TP1100"
+    },
+    "message": "操作成功"
+}
+
+
+/nms/spring/deviceConfig/71f712f9-7fc7-4029-868d-48a4e42597e6/getDeviceBasicParam?token=0B96DD977EAA01B3008058C955B5D6F8
+
