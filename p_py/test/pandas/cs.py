3bfdc3120e1551ac2ab5b7a3446d4c9087672cb5
fengfeng <huangfeng191@163.com>
2019-10-17 05:50:24 +0800

    a

--- p_py/test/pandas/cs.py
+++ p_py/test/pandas/cs.py
@@ -2,9 +2,14 @@
 import sys
 import os, site
 site.addsitedir(os.path.join(os.getcwd(), 'p_py', 'lib'))
+site.addsitedir(os.path.join(os.getcwd(),"..","..", 'lib'))
 import PlatLib  # 添加平台环境变量
 import pandas as pd
 if __name__ == "__main__":
    print "执行的文件名,也算在参数中" 
    df = pd.DataFrame([1, 2, 3, 4, 5], columns=['cols'], index=['a','b','c','d','e'])
+
+   # df1 = pd.DataFrame([2, 4, 6, 4, 5], columns=['cols'], index=['a', 'b', 'c', 'd', 'e'])
+
+
    print df
