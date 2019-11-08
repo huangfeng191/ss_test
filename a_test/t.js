2a26cf4c809563f225f489b5b63113255f71868f
jinjianwu <huangfeng191@163.com>
2019-11-08 11:45:27 +0800

    a

--- a_test/t.js
+++ a_test/t.js
@@ -1,79 +1,47 @@
-$("#in_activeAlarmId").val("");
-$("#in_activeAlarmDesc").val("");
-$("#in_alarmID").val("");
+The specified input was Disqualified or Qualified as a possible system reference.
 
-
-
-$("#in_activeAlarmId").data("data",{})
-
-
-
-err_incomplete_chunked_encoding 
-
-
-max_num
-
-max_monthp
-
-
-    感谢测试组同事对上一个版本的测试。 
-    针对测试组同事提出的问题，我们经过一段时间的开发，
-修复了大部分问题。现在发布一个新的版本，版本号为：1.1.0,
-改版本主要修复了以下问题：
-    1 系统安全相关功能
-    2 链路相关功能
-    3 拓扑管理相关功能
-    4 帮助相关功能
-    5 运维管理相关功能
-    6 告警管理相关功能
-    7 SM2000_GN 接收机相关功能
-    8 SM2000 性能管理相关功能
-    9 管网检测-数据库备份
-还未修复的问题主要包括：
-    1 针对tp1100设备信息的管理及维护
-    2 SM2000设备的升级及网管系统的自动部署
-
-
-
-
-    	
-1 帮助管理bug修改
-
-
-1 性能管理bug修改 2 运维管理bug修改 3 帮助管理bug修改 4 运维管理用户、告警统计日志统计bug修改 5 系统测试，bug修改
+作为一个可能的输入源，
 
 
 
 
 
-
-
-    
-
-
-
-
-
-测试服务器地址： http://192.168.1.227:16080 
-
-
-  
+The fault condition must be continuously present for the Fault Delay Time (FLTDELAY) before disqualifying the input as a possible reference.
+在不符合条件的输入源作为参考之前，错误情况必须是持续显示错误的延迟时间。
+ After INPDISQ is set the alarm clears when the input signal is fault free for the Clear Delay Time(CLRDELAY).
 
 
 
-var rules = {
-    userName: { "length": 50, "err": "用户名长度<=50" },
-    description: { "length": 50, "err": "描述长度<=50" },
-  }
 
-  var verified = top.SsComm.getVerify(data, rules);
-  delete data.userLevel;
-  delete data.roleLevel;
-  if (verified) {
-    $('#myModalTips').html(window.top.showTips(verified));
-    $('#myModalTips').modal('show');
-    return;
-  }
+The specified input port has a excessive    phase measurement     that disqualifies it from being used.
 
-  #  asdf2 1
-  sdf sdf sdf
+输入端口 
+
+超过 phase 测量
+
+从他被使用
+
+指定的输入端口有大量phase测量，用于在检测时间内检测端口是否符合标准
+
+
+
+
+Input has had excessive discontinuities.
+
+输入信号含有大量缺失
+ 
+This is indicated when there are more than 3 signals faults of the same type within a 5-minute window.
+ 
+ The alarm clears when the 5- minute window contains less than 3 alarms of the same type.
+
+
+
+ 
+
+ The specified input port’s calculated received frequency is exceeding the pull-in range of the local oscillator.
+ 
+ 
+ The received signal’s calculated frequency alarmed condition disqualifies the input as a possible reference.
+ 
+ 
+ After INPFRQ is set, the alarm clears when the input frequency is within the defined pull-in range limits.
