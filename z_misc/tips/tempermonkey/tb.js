793e24c1d311467ba339522a8ed54e1823d6a810
fengfeng <huangfeng191@163.com>
2019-12-17 16:17:29 +0800

    admin

--- z_misc/tips/tempermonkey/tb.js
+++ z_misc/tips/tempermonkey/tb.js
@@ -1,29 +1,43 @@
-
-
 // 获取需要点击的按钮
-function getDoDom({text='立即购买'} ) {
-    let dom=null;
-    let a=document.querySelectorAll("a[role='button']");
-    a.forEach(function(v){if(v.innerText==text){dom=v }})
-    console.log("dom"+dom)
+function getDoDom({
+    text = '立即购买'
+}) {
+    let dom = null;
+    let a = document.querySelectorAll("a[role='button']");
+    a.forEach(function (v) {
+        if (v.innerText == text) {
+            dom = v
+        }
+    })
+    console.log("dom" + dom)
     return dom
 }
 
 
 // 查找需要点击的按钮，如果存在则自动点击 不需要循环执行
-function doGetOne({dom=null,time=10,text='立即购买'}) {
+function doGetOne({
+    dom = null,
+    time = 10,
+    text = '立即购买'
+}) {
 
-    if(dom){
+    if (dom) {
         dom.click();
-    }else{
-        var timer=setTimeout(() => {
+    } else {
+        var timer = setTimeout(() => {
 
-            let fDom=getDoDom(text)
-            if(fDom){
-                debugger;
-                  fDom.click();
-            }else{
-                doGetOne({dom:fDom,time:time,text:text} )
+            let fDom = getDoDom({
+                text: text
+            })
+            if (fDom) {
+                //  debugger;
+                fDom.click();
+            } else {
+                doGetOne({
+                    dom: fDom,
+                    time: time,
+                    text: text
+                })
             }
         }, time);
 
@@ -31,27 +45,34 @@ function doGetOne({dom=null,time=10,text='立即购买'}) {
 
     }
 }
+debugger
 // 执行脚本
-if(window.location.href.indexOf("item.htm")>=0){
+if (window.location.href.indexOf("item.htm") >= 0) {
     //  setTimeout(function(){
-      doGetOne({})
+    doGetOne({})
     //  },100);
-  }
-
-
-
-
+} else if (window.location.href.indexOf("order") >= 0) {
+    // debugger;
+    //  setTimeout(function(){
+    doGetOne({
+        "text": "提交订单"
+    })
+    //  },100);
+}
 
 //↓↓↓↓↓↓↓***************  不需要 
 
-function doOneTime({text='立即购买',time=100}) {
-    let dom =getDoDom({});
-    if(dom){
+function doOneTime({
+    text = '立即购买',
+    time = 100
+}) {
+    let dom = getDoDom({});
+    if (dom) {
         setTimeout(() => {
             dom.click();
-        },time)
-       
+        }, time)
+
     }
-    
+
 }
 doOneTime({});
