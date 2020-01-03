f86728ff11d9d87c77f529f6bd899788b21eaf5e
fengfeng <huangfeng191@163.com>
2020-01-03 10:59:52 +0800

    as

---
+++ new_nms/allocation/configure/doing.js
@@ -0,0 +1,4 @@
+if (top.SsCenter.deviceInfo.current && top.SsCenter.deviceInfo.current.deviceType != "SM2000" && top.SsCenter.deviceInfo.current.deviceType !=
+    "SM2000_GN") {
+    $("#item-box-output #config-1").css("display", "block");
+}
---
+++ new_nms/allocation/configure/doing.md
@@ -0,0 +1,3 @@
+ if(top.SsCenter.deviceInfo.current&&top.SsCenter.deviceInfo.current.deviceType!="SM2000" &&top.SsCenter.deviceInfo.current.deviceType!="SM2000_GN"){
+          $("#item-box-output #config-1").css("display","block");
+        }
