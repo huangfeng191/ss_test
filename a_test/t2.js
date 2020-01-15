a8e0b5ba5b94805b7d3600a0de2b198598db21ae
fengfeng <huangfeng191@163.com>
2020-01-15 22:23:36 +0800

    admin

--- a_test/t2.js
+++ a_test/t2.js
@@ -1,1 +1,21 @@
-b = { "areaId": "7d965d07-4b27-44dc-8a02-ce7aebc62619", "ip": "192.168.1.178", "data": "{\"possion\":{\"left\":\"240px\",\"top\":\"110px\"},\"targetIds\":[{\"port\":\"2\",\"slot\":\"2\",\"_port\":\"1\",\"_slot\":\"1\",\"ip\":\"192.168.1.110\"},{\"port\":\"4\",\"slot\":\"2\",\"_port\":\"3\",\"_slot\":\"1\",\"remark\":\"5\",\"ip\":\"192.168.1.123\"}]}" }
+配置域名
+
+
+curl  https://get.acme.sh | sh
+apt-get install -y socat
+
+
+
+sudo ~/.acme.sh/acme.sh --issue -d xyzdogoli.xyz --standalone -k ec-256  
+sudo ~/.acme.sh/acme.sh --installcert -d xyzdogoli.xyz  --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc 
+
+
+
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install forever -g 
+
+
+unzip stock.zip
+
+U8$ryNMt[bo($MzD
