66ff92344ff813a51b82a937cf81f3770e927d41
fengfeng <huangfeng191@163.com>
2020-07-18 10:34:34 +0800

    a

--- a_test/t2.js
+++ a_test/t2.js
@@ -1,1 +1,14 @@
-_eMap
+{ "code": 100000, "data": [
+{ "ioType": null, "index": 0, "redundancy": null, "card": { "type": "CC", "cardName": null, "active": "yes", "portCount": null, "cardLed": { "freeRun": 0, "active": 1, "lock": 1, "alarm": 2 } } },
+        { "ioType": null, "index": 1, "redundancy": null, "card": null },
+{ "ioType": null, "index": 2, "redundancy": null, "card": { "type": "MC", "cardName": null, "active": "yes", "portCount": null, "cardLed": { "pwrA": 1, "pwrB": 0, "system": 3, "alarm": 1 } } },
+        { "ioType": null, "index": 3, "redundancy": "--", "card": { "type": "4-E1", "cardName": null, "active": "--", "portCount": null, "cardLed": { "sys": 3, "gnss": 1, "port1": 1, "port2": 1, "port3": 1, "port4": 1 } } },
+        { "ioType": null, "index": 4, "redundancy": "--", "card": { "type": "4-E1", "cardName": null, "active": "--", "portCount": null, "cardLed": { "sys": 3, "gnss": 3, "port1": 1, "port2": 1, "port3": 1, "port4": 1 } } },
+        { "ioType": null, "index": 5, "redundancy": "--", "card": { "type": "PTP", "cardName": null, "active": "--", "portCount": null, "cardLed": { "pwr": 1, "sys": 1, "ptp1": 0, "ptp2": 0, "ptp3": 0, "ptp4": 0 } } },
+        { "ioType": null, "index": 6, "redundancy": "enable", "card": { "type": "E1-T1", "cardName": null, "active": "yes", "portCount": null, "cardLed": { "pwr": 1, "enable": 1 } } },
+        { "ioType": null, "index": 7, "redundancy": "enable", "card": null },
+{ "ioType": null, "index": 8, "redundancy": "disable", "card": null },
+{ "ioType": null, "index": 9, "redundancy": "disable", "card": { "type": "E1-T1", "cardName": null, "active": "yes", "portCount": null, "cardLed": { "pwr": 1, "enable": 1 } } },
+        { "ioType": null, "index": 10, "redundancy": "enable", "card": { "type": "E1-T1", "cardName": null, "active": "yes", "portCount": null, "cardLed": { "pwr": 1, "enable": 1 } } },
+        { "ioType": null, "index": 11, "redundancy": "enable", "card": { "type": "E1-T1", "cardName": null, "active": "no", "portCount": null, "cardLed": { "pwr": 1, "enable": 0 } } }
+    ] }
